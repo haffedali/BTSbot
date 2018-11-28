138c336328500b48a12888444524e6990d2c1db7
Hoff <haffedmcnair@gmail.com>
2018-11-28 15:22:11 -0800

    posts pokemon related tweets

---
+++ bot.js
@@ -0,0 +1,56 @@
+var twit = require("twit")
+var config = require("./config.js")
+var fetch = require("node-fetch")
+var twitter = new twit(config)
+
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=BTS&api_key=dc6zaTOxFJmzC"
+var getBois = function(){
+    fetch(queryURL)
+    .then((resp) => resp.json())
+    .then(function(resp){
+        console.log(resp.data[3])
+    })
+    .catch(function(error){
+        console.log("oops, error. Here take this " + error)
+    })
+}
+
+
+var newBois = function(gif){
+    twitter.post('media/upload')
+}
+
+
+var retweet = function() {
+    var params = {
+      q: '#pokemon',
+      result_type: 'recent',
+      lang: 'en'    
+    }
+    
+    twitter.get('search/tweets', params, function(err, data){
+        if (!err){
+            //grab ID of tweet
+            var retweetId = data.statuses[0].id_str;
+            //Tell TWITTER to retweet
+            twitter.post('statuses/retweet/:id', {
+                id: retweetId
+            }, function(err, response){
+                if (response){
+                    console.log("retweeted the boys!")
+                }
+                if (err){
+                    console.log(err)
+                    console.log("uh-oh... error trying to post")
+                }
+            })
+        }
+        else {
+            console.log(err)
+        }
+    })
+}
+
+retweet();
+setInterval(retweet, 100000)
---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+    consumer_key: 'GUKZnLzY8ULglUP1wt5feAEw0',  
+    consumer_secret: 'hJ0tSE7prfEFAJH7inj7dCfqIoWSKD4JGZT2S2xphYmwQJepER',
+    access_token: '1067884008841732096-fEjl9CPggr6SoLEF7NKavH5gqukWKy',  
+    access_token_secret: 'oXTDkUZkgsWGdrB3EkcsMQ7u4pe4PoB1S9YVKFdEkDxyb'
+  }
